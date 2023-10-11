3b773837118f4db06f2f614fa677b74d1eb48677
firendly <firendly@iMacBook.local>
2023-10-11 07:48:58 +0330

    🚀 Add phone_number field.

--- organizer/models.py
+++ organizer/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import RegexValidator
 from django.db import models as dModels
 from django.utils.translation import gettext_lazy as _
 
@@ -7,12 +8,16 @@ from cities.models import Ostan, Shahr
 
 class Organizer(iModels.GeneralModel):
     """ Model definition of Organizer. """
+    phone_number = dModels.CharField(
+        _(_('Phone number')),
+        max_length = 16,
+        validators = [RegexValidator(r"^(?:0|98|\+98|\+980|0098|098|00980)?(9\d{9})$")] # TODO: Add validator to separate file.
+    )
     city         = dModels.ForeignKey(Shahr, on_delete=dModels.CASCADE, related_name='organizers')
     province     = dModels.ForeignKey(Ostan, on_delete=dModels.CASCADE, related_name='organizers')
     date_joined  = dModels.DateTimeField(_('Date Joined'), auto_now_add=True)
     description  = dModels.TextField(_('Organizer Description'))
     email        = dModels.EmailField(_('Email'), max_length=254)
-    phone_number = dModels.CharField(_(_('Phone number')), max_length=16)
     website      = dModels.URLField(_('Website URL'), max_length=200, help_text=_('https://www.alibaba.com/'))
 
     def __str__(self):
