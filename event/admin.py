1f49475d033370e2d845569d81b6d5f40fbefe75
firendly <firendly@iMacBook.local>
2023-10-11 07:07:37 +0330

    🚀 Add Event model.

--- event/admin.py
+++ event/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
+from . import models
+
+
 # Register your models here.
+# admin.site.register(models.Event)
+admin.site.register(models.Category)
+admin.site.register(models.Tag)
+admin.site.register(models.Ticket)
+
+
+#admin.py
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('title', 'event_date',)
+
+admin.site.register(models.Event, EventAdmin)
